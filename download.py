f58caf8d7d8d815b208fa251016e22fd91ed25c3
Ricardo Manzano <ricardoamsrams@gmail.com>
2024-10-31 09:56:56 -0400

    Download program

---
+++ download.py
@@ -0,0 +1,5 @@
+from huggingface_hub import snapshot_download
+
+snapshot_download(repo_id="meta-llama/Llama-3.2-11B-Vision-Instruct",
+                  use_auth_token="your token",
+                  local_dir="C:\\pathToSaveTheModel\\")
